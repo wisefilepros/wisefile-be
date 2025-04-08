7c7944f3e6eaae698ab7968c1b7d0137135c89ea
Ben <ben.a.burgos@gmail.com>
2025-04-07 21:15:20 -0700

    adds logout route

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { loginUser, refreshToken } from '../controllers/authController.js';
+import { loginUser, refreshToken, logoutUser } from '../controllers/authController.js';
 
 const router = express.Router();
 
 router.post('/login', loginUser);
 router.post('/refresh', refreshToken);
+router.post('/logout', logoutUser);
 
 export default router;
