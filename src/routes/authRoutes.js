11cb5d6a0be3a9ac0c7e768e3326b464de8aecf6
Ben <ben.a.burgos@gmail.com>
2025-04-07 21:13:22 -0700

    adds refresh route

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import { loginUser } from '../controllers/authController.js';
+import { loginUser, refreshToken } from '../controllers/authController.js';
 
 const router = express.Router();
 
 router.post('/login', loginUser);
+router.post('/refresh', refreshToken);
 
 export default router;
