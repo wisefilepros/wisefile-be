05bb8e813715a8701ae0099ddfe96a27b1e68ab8
Ben <ben.a.burgos@gmail.com>
2025-04-11 14:15:29 -0700

    removes auth from create in testing

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -6,7 +6,8 @@ const router = express.Router();
 
 router.get('/', requireAuth, controller.getAllUsers);
 router.get('/:id', requireAuth, controller.getUserById);
-router.post('/', requireAuth, controller.createUser);
+// router.post('/', requireAuth, controller.createUser);
+router.post('/', controller.createUser);
 router.patch('/:id', requireAuth, controller.updateUser);
 router.delete('/:id', requireAuth, controller.deleteUser);
 
