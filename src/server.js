df8a69bf52c6c897e92bffe67169903e6502055e
Ben <ben.a.burgos@gmail.com>
2025-04-11 15:46:34 -0700

    mounts activitylog

--- src/server.js
+++ src/server.js
@@ -14,6 +14,7 @@ import documentRoutes from './routes/documentRoutes.js';
 import messageRoutes from './routes/messageRoutes.js';
 import feeRoutes from './routes/feeRoutes.js';
 import invoiceRoutes from './routes/invoiceRoutes.js';
+import activityLogRoutes from './routes/activityLogRoutes.js';
 
 const app = express();
 
@@ -43,6 +44,7 @@ app.use('/api/documents', documentRoutes);
 app.use('/api/messages', messageRoutes);
 app.use('/api/fees', feeRoutes);
 app.use('/api/invoices', invoiceRoutes);
+app.use('/api/activity-logs', activityLogRoutes);
 
 // Start server
 app.listen(config.port, () => {
