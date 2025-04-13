7fd0d0456201d15c252a2fa53c6b3a0b13cc8984
Ben <ben.a.burgos@gmail.com>
2025-04-12 21:18:38 -0700

    updates caserecord model

--- src/models/CaseRecord.js
+++ src/models/CaseRecord.js
@@ -12,14 +12,38 @@ const CaseRecordDefinition = {
   primary_contact_id: String,
   status: String,
   sub_status: String,
-  courtCaseNumber: String,
-  courtName: String,
+  court_case_number: String,
+  court_name: String,
   court_decision: String,
   start_date: Date,
   end_date: Date,
   is_active: Boolean,
+  is_temporary: Boolean,
   description: String,
   internal_notes: [{ note: String, user_id: String, date: Date }],
+  rentFeesClaims: {
+    filingPoNumber: String,
+    baseRent: Number,
+    holdover: Boolean,
+    monthsUnpaid: Number,
+    currentMonthUnpaidDate: Date,
+    isSubsidized: Boolean,
+    rentalReliefApplication: Boolean,
+    lateFee: Number,
+    lateMonths: Number,
+    filingFee: Number,
+    miscDebts: [{ description: String, amount: Number }]
+  },
+  acknowledgment: {
+    rentalReliefConfirmed: Boolean,
+    statementsConfirmed: Boolean
+  },
+  missing_attachments_reason: {
+    lease: { selectedOverride: String, reason: String },
+    ledger: { selectedOverride: String, reason: String },
+    demand: { selectedOverride: String, reason: String },
+    deed: { selectedOverride: String, reason: String }
+  }
 };
 
 const CaseRecordSchema = createSchema(CaseRecordDefinition);
